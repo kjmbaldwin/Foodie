5226fa2fe5bb38f5ca65669ebb2d8bd3193429f5
Kyle Baldwin <kylejmbaldwin@gmail.com>
2019-10-02 15:23:25 -0400

    initial commit

---
+++ Foodie.Data/Services/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Foodie.Data.Models;
+
+namespace Foodie.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
---
+++ Foodie.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Foodie.Data.Models;
+
+namespace Foodie.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Some French Name", Cuisine = CuisineType.French },
+                new Restaurant { Id = 3, Name = "Mango Grove", Cuisine = CuisineType.Indian}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
